75338e650c304ec336bd21d99c0fcc42755fb046
William Le <w.le@acaprojects.com>
2018-11-08 18:32:34 +1100

    Entrypoint: remove cotag, new Gemfile.lock strategy

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,28 +1,13 @@
 #!/bin/bash
 set -e
 
-cp -f ../Gemfile.lock.from-docker-build Gemfile.lock 2>/dev/null
+cd /home/aca-apps/ruby-engine-app
+(ls -l ../Gemfile.lock.from-docker-build && cp -f ../Gemfile.lock.from-docker-build Gemfile.lock) || echo "Docker image's Gemfile.lock not found. Using engine repo's"
 
 if [ "$1" == "engine" ]; then
-    cd /home/aca-apps/ruby-engine-app
     exec bundle exec sg -p 8080 -e production
-
 elif [ "$1" == "engine-dev" ]; then
-    cd /home/aca-apps/ruby-engine-app
     exec bundle exec sg -p 8080 -e development
-
-elif [ "$1" == "cotag" ]; then
-    cd /home/aca-apps/cotag-api
-    exec bundle exec sg -p 8080 -e production
-
-elif [ "$1" == "tasks" ]; then
-    cd /home/aca-apps/cotag-api
-    exec bundle exec sidekiq -q default
-
-elif [ "$1" == "processing" ]; then
-    cd /home/aca-apps/cotag-api
-    exec bundle exec sidekiq -q transcode -c $2
-    
 else
     # Else, just run the command that we were given
     exec "$@"
