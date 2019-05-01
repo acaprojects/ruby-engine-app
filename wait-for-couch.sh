c7eab6b13b55cd285c92c02181a18125cfb90705
Viv B <viv@acaprojects.com>
2019-05-01 13:18:42 +1000

    Update wait-for-couch.sh

--- wait-for-couch.sh
+++ wait-for-couch.sh
@@ -1,17 +1,22 @@
-#!/bin/sh
-
-couchup=1
-
-while [ "$couchup" -gt 0 ]; do
-  >&2 echo "Waiting for Couch to become available"
-  echo $couchup
-  sleep 1
-  couchup=`curl -u admin:$COUCHBASE_PASSWORD http://couch:8091/pools/default/buckets | grep -c unhealthy`;
-  couchup="$(($couchup + 0))"
-  echo $couchup
-done
-
->&2 echo "Couch is up - starting engine"
-set -e
-cd /home/aca-apps/ruby-engine-app
-exec bundle exec sg -p 8080 -e production
+#!/bin/bash 
+ 
+if [ "$1" == "engine" ]; then 
+        couchup=1 
+ 
+        while [ "$couchup" -gt 0 ]; do 
+          >&2 echo "Waiting for Couch to become available - wait-for-couch.sh" 
+          echo $couchup 
+          sleep 1 
+          couchup=`curl -s -u admin:$COUCHBASE_PASSWORD http://couch:8091/pools/default/buckets | grep -c  
+unhealthy`; 
+          couchup="$(($couchup + 0))" 
+          echo $couchup 
+        done 
+ 
+        >&2 echo "Couch is up - starting engine" 
+        cd /home/aca-apps/ruby-engine-app 
+        set -e 
+        exec bundle exec sg -p 8080 -e production 
+else 
+        exec "$@" 
+fi
